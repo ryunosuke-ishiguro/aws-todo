50969c401f1561a6b21bb8f49e402e69b2c4c155
Ryunosuke Ishiguro <ryunosuke_ishiguro@demia.co.jp>
2024-12-19 20:55:13 +0900

    .gitignoreの編集

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const ApiUrls = {
+    getTodo:
+        "https://htjzxx9uba.execute-api.ap-northeast-1.amazonaws.com",
+    manageTodo:
+        "https://bndcb5zm34.execute-api.ap-northeast-1.amazonaws.com",
+};
