482d805d4c30889ef15cd873abb1d21ce6c9aff1
Ryunosuke Ishiguro <ryunosuke_ishiguro@demia.co.jp>
2024-12-19 22:19:02 +0900

    config.jsの修正

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 export const ApiUrls = {
     getTodo:
-        "https://htjzxx9uba.execute-api.ap-northeast-1.amazonaws.com",
+        "https://htjzxx9uba.execute-api.ap-northeast-1.amazonaws.com/default/GetTodo",
     manageTodo:
-        "https://bndcb5zm34.execute-api.ap-northeast-1.amazonaws.com",
+        "https://bndcb5zm34.execute-api.ap-northeast-1.amazonaws.com/default/ManageTodo",
 };
